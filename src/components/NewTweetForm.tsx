9101d32c9be4ce15633e6d9903bcad6146b49897
ronit18 <ronitgandhi96@gmail.com>
2023-06-06 15:03:43 +0530

    Created NewTweetForm component.

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useCallback, useLayoutEffect, useRef, useState } from "react";
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
 import { useSession } from "next-auth/react";
 
-const NewTweetForm = () => {
+const updateTextAreaSize = (textArea: HTMLTextAreaElement) => {
+  if (textArea == null) return;
+
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+};
+
+const Form = () => {
   const session = useSession();
 
-  if (session.status !== "authenticated") return;
+  const [inputValue, setInputValue] = useState("");
 
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+
+  if (session.status !== "authenticated") return null;
   if (session.data.user.image == null || session.data.user.image == undefined) {
     session.data.user.image = "/placeholder.jpg";
   }
@@ -17,6 +37,9 @@ const NewTweetForm = () => {
       <div className="flex gap-4">
         <ProfileImage className="" src={session.data.user.image} />
         <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          onChange={(e) => setInputValue(e.target.value)}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?!"
         />
@@ -24,6 +47,11 @@ const NewTweetForm = () => {
       <Button className="self-end">Tweet</Button>
     </form>
   );
+};
+const NewTweetForm = () => {
+  const session = useSession();
+  if (session.status !== "authenticated") return null;
+  return <Form />;
 };
 
 export default NewTweetForm;
