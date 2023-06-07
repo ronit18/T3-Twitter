e103179f10277118486e1b19c82a6056dcb023b1
ronit18 <ronitgandhi96@gmail.com>
2023-06-07 11:23:04 +0530

    Added styling to sidenav buttons.

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,6 +1,10 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import Button from "./Button";
+import { AiFillHome } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { BiLogInCircle, BiLogOutCircle } from "react-icons/bi";
 
 const SideNav = () => {
   const session = useSession();
@@ -10,20 +14,38 @@ const SideNav = () => {
     <nav className="sticky top-0 px-2 py-4">
       <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
         <li>
-          <Link href="/">Home</Link>
+          <Button gray className="flex items-center gap-3">
+            <AiFillHome size={18} />
+            <Link href="/">Home</Link>
+          </Button>
         </li>
         {user != null && (
           <li>
-            <Link href={`/profiles/${user.id}`}>Profile</Link>
+            <Button gray className="flex items-center gap-3">
+              <CgProfile size={18} />
+              <Link href={`/profiles/${user.id}`}>Profile</Link>
+            </Button>
           </li>
         )}
         {user == null ? (
           <li>
-            <button onClick={() => void signIn()}>Log In</button>
+            <button
+              className="flex items-center gap-3"
+              onClick={() => void signIn()}
+            >
+              <BiLogInCircle size={18} />
+              Log In
+            </button>
           </li>
         ) : (
           <li>
-            <button onClick={() => void signOut()}>Log Out</button>
+            <button
+              className="flex items-center gap-3"
+              onClick={() => void signOut()}
+            >
+              <BiLogOutCircle size={18} />
+              Log Out
+            </button>
           </li>
         )}
       </ul>
